59bc967f7756f37a174c0d12ce2806f1d57791ce
Cory Pollard <astandard87@gmail.com>
2022-06-12 13:08:02 -0600

    Minor edits to styling

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const toTop = document.querySelector('.to-top');
 const header = document.querySelector('header');
+const about = document.querySelector('.section__about');
 
 function addToTopBtnHandler() {}
 addToTopBtnHandler();
@@ -10,19 +11,8 @@ toTop.addEventListener('click', function () {
   window.scrollTo(0, 0);
 });
 
-function addToTop() {
-  console.log('yo');
-  toTop.classList.remove('hidden');
-}
-
-// let options = {
-//   root: document.querySelector('.nav-wrapper'),
-//   rootMargin: '0px',
-//   threshold: 1.0,
-// };
-
 let observer = new IntersectionObserver(entries => {
-  console.log(entries[0].isIntersecting);
+  console.log(entries);
   if (!entries[0].isIntersecting) {
     toTop.classList.remove('hidden');
   } else {
