bbf6f5a26e8bf42c6af3784af49ef5f5d52a13b1
Cory Pollard <astandard87@gmail.com>
2022-12-15 17:15:50 -0600

    Added slide in effect to section headings

--- script.js
+++ script.js
@@ -4,16 +4,15 @@ const toTop = document.querySelector('.to-top');
 const header = document.querySelector('header');
 const about = document.querySelector('.section__about');
 const nav = document.querySelector('.nav-bar');
+const headingSlide = document.querySelectorAll('.heading-slide');
 
+// Scroll to top handler
 function addToTopBtnHandler() {
-  toTop.addEventListener('click', function () {
-    window.scrollTo(0, 0);
-  });
+  toTop.addEventListener('click', () => window.scrollTo(0, 0));
 }
 addToTopBtnHandler();
 
-let observer = new IntersectionObserver(entries => {
-  console.log(entries);
+const toTopObserver = new IntersectionObserver(entries => {
   if (!entries[0].isIntersecting) {
     toTop.classList.remove('hidden');
   } else {
@@ -21,7 +20,7 @@ let observer = new IntersectionObserver(entries => {
   }
 });
 
-observer.observe(header);
+toTopObserver.observe(header);
 
 // Nav bar links
 nav.addEventListener('click', function (e) {
@@ -30,4 +29,23 @@ nav.addEventListener('click', function (e) {
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView();
   }
+});
+
+headingSlide.forEach(heading => {
+  const headingSlideObserver = new IntersectionObserver(
+    entries => {
+      const [entry] = entries;
+      if (entry.isIntersecting) {
+        heading.classList.add('slide-in');
+        heading.style.opacity = 1;
+      }
+      // else headingAbout.classList.remove('slide-in');
+    },
+    {
+      root: null,
+      threshold: 1,
+    }
+  );
+
+  headingSlideObserver.observe(heading);
 });
